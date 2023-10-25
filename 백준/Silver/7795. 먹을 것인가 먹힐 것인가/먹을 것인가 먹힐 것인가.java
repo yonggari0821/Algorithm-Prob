34893b1da702e050ce73e38df44a9986cd520bee
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2023-10-25 23:58:42 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 360 ms, Memory: 38536 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -21,15 +21,17 @@ public class Main {
 			for (int i = 0; i < M; i++) B[i] = Integer.parseInt(st.nextToken());
 			Arrays.sort(A);
 			Arrays.sort(B);
-			int ai = N - 1;
-			int bi = M - 1;
+			int ai = N - 1; // 뒤에서부터 ai를 기준으로
+			int bi = M - 1; // bi도 뒤에서 부터
 			int cnt = 0;
+			// ai를 기준으로 현재 bi보다 큰 상태면 하나씩 빼가면서 현재 bi인덱스 + 1 만큼 갯수로 더해주고
 			while (ai >= 0 && bi >= 0)
 			{
 				while (ai >= 0 && A[ai] > B[bi]) {
 					cnt+=(bi + 1);
 					ai--;
 				}
+				// 작아지면 bi를 줄여보기!
 				while (ai >= 0 && bi >= 0 && A[ai] <= B[bi]) bi--;
 			}
 			ans.append(cnt).append('\n');
