98b8b45c06c8285658e5173f95c91f17d881ce49
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2023-12-25 20:47:38 +0900

    [Gold III] Title: 이상한 배열, Time: 512 ms, Memory: 96560 KB -BaekjoonHub

--- 백준/Gold/28099. 이상한 배열/이상한 배열.java
+++ 백준/Gold/28099. 이상한 배열/이상한 배열.java
@@ -2,107 +2,105 @@ import java.io.*;
 import java.util.*;
 
 /*
-28099번 풀이
-1) 각 숫자가 처음 등장하면 startMap에 두 번 이상 등장하면 endMap에 등장하는 index를 저장해두면서
-   동시에 segmentTree를 구성 (원본 배열 없이 한 번에 함)
-2) segmentTree의 경우 체크 하려는 구간에 더 큰 값이 없는 지 체크해야하므로 최대값 트리로 만들어야 함!
-3) endMap에 있는 숫자들을 기준으로 startMap에서 시작 index를 꺼내서 두 범위에 더 큰 값이 있는 지 체크해서
-   더 큰 값이 하나라도 있으면 isOdd를 false로 만들고 출력
+[ 28099]_이상한 배열_안상준
+
+길이가 N인 배열 A이
+1) Ai = Aj를 만족하는 정수 1 <= i,j <= N과
+2) i < k < j를 만족하는 정수 k에 대해,
+3) 항상 Ak <= Ai를 만족할 경우
+A를 "이상한 배열"이라고 하기로 함
+
+테스트 케이스 최대 20만개
+배열의 길이인 N은 최대 20만개
+
+이상한 배열이면 Yes // 아니면 No를 출력할 것
+
+<풀이>
+먼저 조건 1) 이 의미하는 건 "배열 내에서 값이 같은 원소가 있다면"이고
+해당 원소들 사이에 있는 원소들은 그 같은 값의 원소보다 작거나 같아야 한다는 뜻!
+
+예를 들면
+2 1 1 1 2 => 이상한 배열
+2 1 3 1 2 => 이상하지 않은 배열
+
+1) 같은 숫자가 있음 == 해당 숫자가 2번 이상 등장함 // 모든 수들의 범위는 최소 1, 최대 20만
+=> 따라서 수의 범위인 1 ~ 20만을 원래 배열로 하는 최댓값 세그먼트 트리를 만들고
+2) Map을 사용해서 이미 등장한 적이 있으면 그때의 인덱스와 현재 인덱스 사이의 구간 최댓값 <= 현재 값인지 체크 (그리고 다시 현재 인덱스 집어넣어야 함!)
+=> 한 개라도 아니면 이상하지 않은 배열
+=> 모두 괜찮으면 이상한 배열
+3) 등장한 적이 없으면 그냥 Map에 새로 넣기만 하면 됨
 
  */
 
 
 public class Main {
-	// 출력용
-	static String yesOrNo (boolean isOdd)
+
+	static int[] nums;
+	static int[] segmentMaxTree;
+	static int N, S;
+
+	static int TdInit(int left, int right, int node) // 초기 값 left = 0, right = 원래 배열의 끝 값, node = 1(세그먼트 트리 구조 상 0은 빼고 생각!)
 	{
-		if (isOdd) return "Yes";
-		return "No";
+		if (left > N) return 0; // 관련 없는 곳은 관련 없는 값을 return
+
+		// 여기 부턴 관련이 조금이라도 있는 구간
+		if ( left != right ) // 만약 내부 노드라면
+		{
+			int mid = (left + right) / 2;  // 중앙값을 구해서
+			segmentMaxTree[node] = Math.max(TdInit(left, mid, node * 2), TdInit(mid + 1, right, node * 2 + 1)); // 자기 왼쪽 자식과 오른쪽 자식의 연산 값을 리턴
+		}
+		else segmentMaxTree[node] = nums[left]; // 리프 노드라면 바로 그 값을 리턴 // 이 때, 헷갈리는 것은 node와 left // node는 현재 실질적으로는 세그먼트 트리 배열의 절반 사이즈를 left에 더한 값임 // left는 말 그대로 현재 node가 참조하고 있는 범위의 왼쪽 경계 값임!
+		return segmentMaxTree[node];
 	}
 
-	// 쿼리 : 구간에 target보다 더 큰 값이 있는 지 체크하고 있으면 isOdd 를 false로 만드는 함수
-	static void query (int queryLeft, int queryRight, int[] tree)
+	static int TdQuery(int left, int right, int node, int queryLeft, int queryRight)
 	{
-		int val = tree[queryLeft];
-		int target = tree[queryRight];
-		while (queryLeft <= queryRight)
+		if (right < queryLeft || queryRight < left) return 0; // 관련 없는 구간은 관련 없는 값! // 최댓값 트리일 때는 제일 작은 값 // 최소값 트리일 때는 제일 큰 값 // 구간 합 트리일 때는 0 // 구간 곱 트리일 때는 1 등등!
+		else if (queryLeft <= left && right <= queryRight) return segmentMaxTree[node];// 완전히 포함되는 경우 바로 그 값을 가져다 씀
+		else // 부분만 포함되는 경우 => 필요한 부분만 가져다 써야 함 => 계속해서 자식까지 내려가면 결국 온전히 담기거나 아예 관련 없는 부분이 나오므로 자식으로 일단 넘기자!
 		{
-			if ( (queryLeft & 1) != 0 ) val = Math.max(tree[queryLeft++], val);
-			queryLeft /= 2;
-			if ( (queryRight & 1) == 0 ) val = Math.max(tree[queryRight--], val);
-			queryRight /= 2;
+			int mid = (left + right) / 2;
+			return Math.max( TdQuery(left, mid, node * 2, queryLeft, queryRight), TdQuery(mid + 1, right, node * 2 + 1, queryLeft, queryRight) );
 		}
-		if (val > target) isOdd = false;
 	}
 
 	static boolean isOdd;
+
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder ans = new StringBuilder();
 		StringTokenizer st;
+		Map<Integer, Integer> map;
+
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++)
 		{
+			S = 1;
 			isOdd = true;
-			int size = 1;
-			Map<Integer, Integer> startMap = new HashMap<>();
-			Map<Integer, Integer> endMap = new HashMap<>();
-			int N = Integer.parseInt(br.readLine()); // 최대 20만
-			while (size < N) size *= 2;
-			int[] segmentTree = new int[size * 2];
+			map = new HashMap<>();
+			N = Integer.parseInt(br.readLine());
+			nums = new int[N];
+			while (S < N) S *= 2;
+			segmentMaxTree = new int[2 * S];
 			st = new StringTokenizer(br.readLine());
-			for (int i = size; i < size + N; i++)
+			for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+			TdInit(0, N - 1, 1);
+			for (int i = 0; i < N; i++)
 			{
-				segmentTree[i] = Integer.parseInt(st.nextToken());
-				if (!startMap.containsKey(segmentTree[i])) startMap.put(segmentTree[i], i); // 등장한 적 없으면 시작맵에 인덱스와 값 저장
-				else endMap.put(segmentTree[i], i); // 등장한 적이 있다면 끝맵에 저장 // 여러 개면 자동적으로 더 뒤의 것으로 덮어써지므로 상관 없음!
+				int isExist = map.getOrDefault(nums[i], -1);
+				if (isExist != -1)
+				{
+					if (TdQuery(0, N - 1, 1, isExist, i) > nums[i]) {
+						isOdd = false;
+						break;
+					}
+				}
+				map.put(nums[i], i);
 			}
-			// 세그먼트 트리는 최댓값 트리로 구성
-			for (int i = size - 1; i > 0; i--) segmentTree[i] = Math.max(segmentTree[2 * i], segmentTree[2 * i + 1]);
-			
-			
-			/*
-			[Map에 있는 key와 value 탐색하는 법]
-			Map에 있는 key와 value의 쌍을 Entry라고 함
-			----------------------------------------
-			Entry == <key , value>
-			----------------------------------------
-			즉, Map은 이 Entry들의 Set라고 할 수도 있음
-			이 때, Map에 있는 이 Entry 들을 탐색할 수 있는 방법이 있는데, 바로 Iterator라는 클래스를 이용하는 것이다.
-			
-			사용방법은 Iterator를 선언하는 것부터 시작한다.
-			
-			Iterator <Map.Entry<>> iterator = 맵_이름.entrySet().iterator();
-			이 때, Map.Entry<>의 제네릭에는 받아올 Map의 제네릭을 똑같이 넣어주면 된다.
-			
-			그러면 이제 이 iterator라는 객체는 hasNext()와 next()라는 메서드를 가진 탐색용 객체가 된다.
-			hasNext()의 경우 Queue의 isEmpty()와 같은 함수로 Map에 탐색할 다음 Entry가 있는 지를 true/false로 반환해주는 함수이고,
-			Next()의 경우 다음 Entry를 반환하는 함수이다.
-			
-			따라서 우리는 엔트리를 iterator.next()로 받아와서 사용하면 되고, 그 방법은 아래와 같다.
-			while (iterator.hasNext()) {
-				// 엔트리 받아오기
-				Map.Entry<> entry = iterator.next();
-				// 받아온 엔트리에서 key 가져오기 
-				key의_자료형 keyName = entry.getKey();
-				// 받아온 엔트리에서 value 가져오기
-				value_자료형 valueName = entry.getValue();
-			}
-			
-			 */
-			
-			// endMap에 들어있는 수들을 대상으로 startMap에서 같은 수의 value 값과 함께 세그먼트 트리 탐방
-			Iterator<Map.Entry<Integer, Integer>> iterator = endMap.entrySet().iterator();
-			while (iterator.hasNext())
-			{
-				Map.Entry<Integer, Integer> keyAndValue = iterator.next();
-				int num = keyAndValue.getKey();
-				int endIndex = keyAndValue.getValue();
-				query(startMap.get(num), endIndex, segmentTree);
-				if (!isOdd) break;
-			}
-			ans.append(yesOrNo(isOdd));
+			if (isOdd) ans.append("Yes");
+			else ans.append("No");
 			ans.append('\n');
 		}
 		bw.write(ans.toString());
