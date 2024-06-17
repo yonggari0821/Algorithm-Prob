38e0b62164f8d1b2a1f8ce6a0d17007bc94b406a
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-06-17 19:10:53 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 19992 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 public class Main {
 	static int N, M;
+	static int[] pickedNums;
+	static boolean[] used;
 	static StringBuilder ans = new StringBuilder();
-	static void func(int[] pickedNums, boolean[] pickedOrNot, int pickOrder)
+	static void func (int picked)
 	{
-		if (pickOrder == M)
+		if (picked == M + 1)
 		{
-			for (int i = 0; i < pickedNums.length; i++)
+			for (int i = 1; i <= M; i++)
 			{
 				ans.append(pickedNums[i]).append(" ");
 			}
@@ -17,22 +19,23 @@ public class Main {
 		}
 		for (int i = 1; i <= N; i++)
 		{
-			if (pickedOrNot[i]) continue;
-			pickedOrNot[i] = true;
-			pickedNums[pickOrder] = i;
-			func(pickedNums, pickedOrNot, pickOrder + 1);
-			pickedOrNot[i] = false;
+			if (used[i]) continue;
+			used[i] = true;
+			pickedNums[picked] = i;
+			func(picked + 1);
+			used[i] = false;
 		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		int[] pickedNums = new int[M];
-		boolean[] pickedOrNot = new boolean[N+1];
-		func(pickedNums, pickedOrNot, 0);
+		pickedNums = new int[M + 1];
+		used = new boolean[N + 1];
+		func(1);
 		bw.write(ans.toString());
 		br.close();
 		bw.close();
