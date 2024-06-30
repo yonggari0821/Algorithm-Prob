1bb576755af66dd147bb424de2cc107ec9da4abb
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-06-30 21:07:24 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,32 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, a;
-    static StringBuilder ans = new StringBuilder();
-    static void func(int picked, int used, int last)
-    {
-        if (picked == M)
-        {
-            for (int i = 0; i <= N; i++) {
-                if ((used & (1 << i)) != 0) ans.append(i).append(" ");
-            }
-            ans.append('\n');
-            return ;
-        }
-        for (int i = last + 1; i <= N; i++) {
-            func(picked + 1, (used | (1 << i)), i);
-        }
 
+    static void nmFunc(int numAsBool, int lastNum)
+    {
+        if (Integer.bitCount(numAsBool) >= M)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                if ((numAsBool & (1 << i)) != 0) sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = lastNum; i <= N; i++) {
+            nmFunc(numAsBool | (1 << i), i + 1);
+        }
     }
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-        func(0, 0, 0);
-    	bw.write(ans.toString());
-    	br.close();
-    	bw.close();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nmFunc(0, 1);
+        System.out.println(sb.toString());
     }
 }
