fc7053ce43fb4b3dc1f142bbf3edd201afb74a25
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-07-22 18:28:03 +0900

    [Gold V] Title: 괄호의 값, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,60 +1,80 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+
+/*
+백준 2504 괄호의 값
+() 2
+[] 3
+
+괄호 안에 있는 것들은 x
+밖에 있는 것들은 +
+로 생각하기
+
+올바르지 못한 입력 값인 경우(괄호의 쌍이 맞지 않는 경우, 여는 괄호 없이 닫는 괄호가 먼저 생긴 경우 등)는 0 출력하기
+
+ex)  () ()
+
+<풀이>
+괄호의 쌍이 맞아야 함 => 닫는 괄호가 나왔을 때 이전 값이 해당 괄호와 맞는 쌍이어야 함.
+
+최대 길이가 30이므로 최대 값은 3의 15승이므로 int형으로 해도 무방
+
+최종 값 / 곱해지는 값을 위한 2개의 값을 활용
+ */
+
 public class Main {
-    static Stack<Character> stack;
-    static int N, ans = 0, tval = 1;
-    public static void main(String[] args) throws IOException {
+    static int isOpenBracket(char bracket)
+    {
+        if (bracket == '(') return 2;
+        else if ( bracket == '[') return 3;
+        else if ( bracket == ')') return -2;
+        else if ( bracket == ']') return -3;
+        return 0;
+    }
+
+    public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        stack = new Stack<>();
-        String str = br.readLine();
-        for (int i = 0; i < str.length(); i++)
-        {
-            char tmp = str.charAt(i);
-            if (tmp == '(' || tmp == '[') // 여는 괄호
+        String brackets = br.readLine();
+        Stack<Character> bracketStack = new Stack<>();
+        int res = 0; // 최종 결과 값
+        int multiple = 1; // 현재 곱해진 값 // 곱셈이므로 기본 값 1
+
+        for (int i = 0; i < brackets.length(); i++) {
+            // 현재 괄호 및 값
+            char currentBracket = brackets.charAt(i);
+            int currentVal = isOpenBracket(currentBracket);
+
+            // 여는 괄호
+            if (currentVal > 1)
             {
-                stack.push(tmp);
-                if (tmp == '(') tval *= 2;
-                else tval *= 3;
+                multiple *= currentVal;
+                bracketStack.push(currentBracket);
             }
-            else // 닫는 괄호
-            {
-                if (stack.isEmpty())
-                {
-                    ans = 0;
-                    break;
+
+            // 닫는 괄호
+            else {
+                // 비어 있거나 짝이 맞지 않는 경우 => 올바르지 않으므로 0 출력하고 끝내기
+                if (bracketStack.isEmpty() || isOpenBracket(bracketStack.peek()) + currentVal != 0) {
+                    System.out.println(0);
+                    return ;
                 }
-                else
-                {
-                    char pair = stack.pop();
-                    if (tmp == ')' && pair == '(')
-                    {
-                        if (str.charAt(i - 1) == '(')
-                            ans += tval;
-                        tval /= 2;
-                    }
-                    else if (tmp == ']' && pair == '[')
-                    {
-                        if (str.charAt(i - 1) == '[')
-                            ans += tval;
-                        tval /= 3;
-                    }
-                    else
-                    {
-                        ans = 0;
-                        break;
-                    }
+                // 짝이 맞는 경우
+                else {
+                    // 바로 앞의 괄호를 확인해서 닫는 여는 괄호인 경우 해당 번째 까지의 값이 최종 값에 더해지면 되고 // 즉 괄호로 묶인 것들은 가장 안쪽 값이 반영되는 것!
+                    if (isOpenBracket(brackets.charAt(i - 1)) > 0) res += multiple;
+                    // 나머지의 경우 그냥 해당 짝의 값으로 나눠주면 됨.
+                    multiple /= isOpenBracket(bracketStack.pop());
                 }
             }
-            // System.out.println(ans);
         }
 
-        if (!stack.isEmpty()) bw.write('0');
-        else
-            bw.write(String.valueOf(ans));
-        br.close();
-        bw.flush();
-        bw.close();
+        // 다 했는데 스택이 비어있지 않은 경우 => 올바르지 않은 경우
+        if (!bracketStack.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(res);
     }
 }
+
