8bb9b96a9c2d0bf3d11204d5bcb0187216912d42
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-08-08 23:38:03 +0900

    [Gold I] Title: 구간 합 구하기, Time: 564 ms, Memory: 102812 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,105 +1,128 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
+public class Main {
+    static int N, M, K, S = 1;
+    static StringBuilder ans = new StringBuilder();
+    static long[] nums;
+    static long[] tree;
 
-class FenwickTree {
-    long[] tree;
-
-    public FenwickTree(int n) {
-        this.tree = new long[n + 1];
+    // [Top-Down]
+    static long TDinit(int start, int end, int node) // (1, S, 1)
+    {
+        if (start > N) return 0;
+        if (start != end) // 내부 노드
+        {
+            int mid = start + (end - start) / 2;
+            tree[node] = TDinit(start, mid, node * 2) + TDinit(mid + 1, end, node * 2 + 1);
+        }
+        else // 리프 노드
+            tree[node] = nums[node - S + 1];
+        return tree[node];
     }
 
-    public void init(long[] nums) {
-        if (this.tree == null) this.tree = new long[nums.length + 1];
-
-        for (int i = 0; i < nums.length; i++) {
-            add(i + 1, nums[i]);
+    static void TDupdate(int start, int end, int node, int target, long diff)
+    {
+        if (end < target || target < start) return; // 무관한 범위
+        else // 유관한 범위
+        {
+            tree[node] += diff; // 값 반영
+            if (start != end) // 내부 노드
+            {
+                int mid = start + (end - start) / 2;
+                TDupdate(start, mid, node * 2, target, diff);
+                TDupdate(mid + 1, end, node * 2 + 1, target, diff);
+            }
+            // else == 리프 노드 ==> 리프 노드 전 마지막 내부노드에서 이미 값 변경 수행해줄 것이므로 따로 로직 필요 없음!
         }
     }
 
-    public void add(int idx, long val) {
-        for (int i = idx; i < tree.length; i += lowbit(i)) tree[i] += val;
+    static long TDquery (int start, int end, int node, int queryStart, int queryEnd)
+    {
+        if (end < queryStart || queryEnd < start)  return 0; // 무관한 범위
+        else if (queryStart <= start && end <= queryEnd) return tree[node]; // 완전히 포함
+        else // 일부만 포함
+        {
+            int mid = start + (end - start) / 2;
+            long leftValue = TDquery(start, mid, node * 2, queryStart, queryEnd);
+            long rightValue = TDquery(mid + 1, end, node * 2 + 1, queryStart, queryEnd);
+            return leftValue + rightValue;
+        }
     }
 
-    public long query(int idx) {
+    // [Bottom-Up]
+    static void BUinit()
+    {
+        for (int i = 1; i <= N; i++) tree[S + i - 1] = nums[i];
+        for (int i = S - 1; i > 0; i--) tree[i] = tree[i*2] + tree[i*2 + 1];
+    }
+
+    static void BUupdate(int target, long goal)
+    {
+        int node = S + target - 1;
+        tree[node] = goal;
+        node /= 2;
+        while(node > 0)
+        {
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+            node /= 2;
+        }
+    }
+
+    static long BUquery (int queryStart, int queryEnd)
+    {
         long sum = 0;
-        for (int i = idx; i > 0; i -= lowbit(i)) sum += tree[i];
+        int start = queryStart + S - 1;
+        int end = queryEnd + S - 1;
+        while (start <= end)
+        {
+            if (start % 2 == 1)
+                sum += tree[start++];
+            if (end % 2 == 0)
+                sum += tree[end++];
+            start /= 2;
+            end /= 2;
+            // else로 하면 start만 가지고 판단하므로 X
+        }
         return sum;
     }
-
-    public long sectionQuery(int left, int right) {
-        long sum = query(right);
-        return sum -= query(left - 1);
-    }
-
-    private int lowbit(int x) {
-        return x & -x;
-    }
-}
-
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M, K;
-
     public static void main(String[] args) throws IOException {
+        // 구간합 + 값 변경 잦음 => 인덱스 트리 활용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String testCase;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while ((testCase = br.readLine()) != null && !testCase.isEmpty()) {
-            StringTokenizer st = new StringTokenizer(testCase);
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 전체 수 갯수
+        M = Integer.parseInt(st.nextToken()); // 값 변경 횟수
+        K = Integer.parseInt(st.nextToken()); // 쿼리 갯수
 
-            long[] nums = new long[N];
-            FenwickTree tree = new FenwickTree(N);
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                nums[i] = Long.parseLong(st.nextToken());
+        while (S < N) S *= 2; // 인덱스 트리 사이즈 조정
+
+        nums = new long[N+1];
+        tree = new long[2*S];
+
+        for (int n = 1; n <= N; n++)
+            nums[n] = Long.parseLong(br.readLine());
+
+        TDinit(1, S, 1);
+        for (int mk = 1; mk <= M + K; mk++)
+        {
+            st = new StringTokenizer(br.readLine());
+            int updateOrQuery = Integer.parseInt(st.nextToken());
+            int numOrderToUpdate = Integer.parseInt(st.nextToken());
+            long updateTo = Long.parseLong(st.nextToken());
+
+            if (updateOrQuery == 1)
+            {
+                long diff = updateTo - tree[numOrderToUpdate + S - 1];
+                TDupdate(1, S, 1, numOrderToUpdate, diff);
             }
-
-            tree.init(nums);
-
-            for (int i = 0; i < M + K; i++) {
-                st = new StringTokenizer(br.readLine());
-                int addOrQuery = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                long c = Long.parseLong(st.nextToken());
-
-                if (addOrQuery == 1) {
-                    tree.add(b, c - nums[b - 1]);
-                    nums[b - 1] = c;
-                } else {
-                    sb.append(tree.sectionQuery(b, (int) c)).append('\n');
-                }
-            }
+            else if (updateOrQuery == 2)
+                ans.append(TDquery(1, S, 1, numOrderToUpdate, (int) updateTo)).append('\n');
         }
-        System.out.print(sb.toString());
+
+        bw.write(ans.toString());
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
-
-/*
-[1]
-5 2 2
-1
-2
-3
-4
-5
-1 3 6
-2 2 5
-1 5 2
-2 3 3
-
-17
-6
-
-[2]
-4 0 1
-1
-2
-3
-4
-2 1 1
-
-1
- */
