feac68646a606aa514d7823a3ebca0cea2f79949
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-08-23 14:13:09 +0900

    [Gold I] Title: 구간 합 구하기, Time: 528 ms, Memory: 118700 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,128 +1,123 @@
 import java.io.*;
-import java.util.StringTokenizer;
-public class Main {
-    static int N, M, K, S = 1;
-    static StringBuilder ans = new StringBuilder();
-    static long[] nums;
-    static long[] tree;
+import java.util.*;
 
-    // [Top-Down]
-    static long TDinit(int start, int end, int node) // (1, S, 1)
-    {
-        if (start > N) return 0;
-        if (start != end) // 내부 노드
-        {
-            int mid = start + (end - start) / 2;
-            tree[node] = TDinit(start, mid, node * 2) + TDinit(mid + 1, end, node * 2 + 1);
-        }
-        else // 리프 노드
-            tree[node] = nums[node - S + 1];
-        return tree[node];
-    }
+/*
+백준 2042 구간 합 구하기
+펜윅트리 또는 세그먼트 트리를 통해 풀이 가능
+ */
 
-    static void TDupdate(int start, int end, int node, int target, long diff)
-    {
-        if (end < target || target < start) return; // 무관한 범위
-        else // 유관한 범위
-        {
-            tree[node] += diff; // 값 반영
-            if (start != end) // 내부 노드
-            {
-                int mid = start + (end - start) / 2;
-                TDupdate(start, mid, node * 2, target, diff);
-                TDupdate(mid + 1, end, node * 2 + 1, target, diff);
-            }
-            // else == 리프 노드 ==> 리프 노드 전 마지막 내부노드에서 이미 값 변경 수행해줄 것이므로 따로 로직 필요 없음!
+class FenwickTree {
+    long[] tree; // 트리 배열
+
+    public FenwickTree(int n) {
+        this.tree = new long[n + 1];
+    } // 원래 배열의 크기보다 1개 늘려야 n번째 수를 n번 index에 바로 넣을 수 있음
+
+    public void init(long[] nums) {
+        if (this.tree == null) this.tree = new long[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            add(i + 1, nums[i]);
         }
     }
 
-    static long TDquery (int start, int end, int node, int queryStart, int queryEnd)
-    {
-        if (end < queryStart || queryEnd < start)  return 0; // 무관한 범위
-        else if (queryStart <= start && end <= queryEnd) return tree[node]; // 완전히 포함
-        else // 일부만 포함
-        {
-            int mid = start + (end - start) / 2;
-            long leftValue = TDquery(start, mid, node * 2, queryStart, queryEnd);
-            long rightValue = TDquery(mid + 1, end, node * 2 + 1, queryStart, queryEnd);
-            return leftValue + rightValue;
-        }
+    // 더하기 함수
+    /*
+    idx와 val를 받아서 idx 번째에 val만큼을 더하는데, 이 때 lowbit를 활용해서 tree 끝까지 idx번째의 수가 포함되는 모든 자리에 더해줘야 함
+     */
+    public void add(int idx, long val) {
+        for (int i = idx; i < tree.length; i += lowbit(i)) tree[i] += val;
     }
 
-    // [Bottom-Up]
-    static void BUinit()
-    {
-        for (int i = 1; i <= N; i++) tree[S + i - 1] = nums[i];
-        for (int i = S - 1; i > 0; i--) tree[i] = tree[i*2] + tree[i*2 + 1];
-    }
-
-    static void BUupdate(int target, long goal)
-    {
-        int node = S + target - 1;
-        tree[node] = goal;
-        node /= 2;
-        while(node > 0)
-        {
-            tree[node] = tree[node * 2] + tree[node * 2 + 1];
-            node /= 2;
-        }
-    }
-
-    static long BUquery (int queryStart, int queryEnd)
-    {
+    // 쿼리 함수 (구간합 구하기 함수)
+    /*
+    idx를 받아서 해당 idx까지의 값을 구하는 함수
+    더하기 함수와 마찬가지로 lowbit를 활용함 
+    
+    보통 구간합을 구하기 위해서는 (A부터 B까지의 구간합을 구해야 한다고 가정)
+    query(B) - query(A-1)의 방식으로 구한다. => 아래 sectionQuery에 구현해 둠!
+     */
+    public long query(int idx) {
         long sum = 0;
-        int start = queryStart + S - 1;
-        int end = queryEnd + S - 1;
-        while (start <= end)
-        {
-            if (start % 2 == 1)
-                sum += tree[start++];
-            if (end % 2 == 0)
-                sum += tree[end++];
-            start /= 2;
-            end /= 2;
-            // else로 하면 start만 가지고 판단하므로 X
-        }
+        for (int i = idx; i > 0; i -= lowbit(i)) sum += tree[i];
         return sum;
     }
-    public static void main(String[] args) throws IOException {
-        // 구간합 + 값 변경 잦음 => 인덱스 트리 활용
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 전체 수 갯수
-        M = Integer.parseInt(st.nextToken()); // 값 변경 횟수
-        K = Integer.parseInt(st.nextToken()); // 쿼리 갯수
+    public long sectionQuery(int left, int right) {
+        long sum = query(right);
+        return sum -= query(left - 1);
+    }
 
-        while (S < N) S *= 2; // 인덱스 트리 사이즈 조정
-
-        nums = new long[N+1];
-        tree = new long[2*S];
-
-        for (int n = 1; n <= N; n++)
-            nums[n] = Long.parseLong(br.readLine());
-
-        TDinit(1, S, 1);
-        for (int mk = 1; mk <= M + K; mk++)
-        {
-            st = new StringTokenizer(br.readLine());
-            int updateOrQuery = Integer.parseInt(st.nextToken());
-            int numOrderToUpdate = Integer.parseInt(st.nextToken());
-            long updateTo = Long.parseLong(st.nextToken());
-
-            if (updateOrQuery == 1)
-            {
-                long diff = updateTo - tree[numOrderToUpdate + S - 1];
-                TDupdate(1, S, 1, numOrderToUpdate, diff);
-            }
-            else if (updateOrQuery == 2)
-                ans.append(TDquery(1, S, 1, numOrderToUpdate, (int) updateTo)).append('\n');
-        }
-
-        bw.write(ans.toString());
-        br.close();
-        bw.flush();
-        bw.close();
+    // 최하위 비트를 구하는 함수
+    private int lowbit(int x) {
+        return x & -x;
     }
 }
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String testCase;
+
+        while ((testCase = br.readLine()) != null && !testCase.isEmpty()) {
+            StringTokenizer st = new StringTokenizer(testCase);
+            N = Integer.parseInt(st.nextToken()); // 수의 갯수 1~100만
+            M = Integer.parseInt(st.nextToken()); // 수 변경 횟수 1~10000
+            K = Integer.parseInt(st.nextToken()); // 구간합 구하는 갯수 1~10000
+
+            long[] nums = new long[N]; // 원래 수 배열 // 입력으로 주어지는 모든 수는 -263보다 크거나 같고, 263-1보다 작거나 같은 정수이다. 라고 했으므로 long으로 선언
+            FenwickTree tree = new FenwickTree(N);
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                nums[i] = Long.parseLong(st.nextToken());
+            }
+
+            tree.init(nums);
+
+            for (int i = 0; i < M + K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int addOrQuery = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                long c = Long.parseLong(st.nextToken());
+
+                if (addOrQuery == 1) {
+                    tree.add(b, c - nums[b - 1]); // 펜윅트리의 b번째에, 바꾸려는 값에서 원래 배열의 b번째(nums[b-1]) 값을 뺀 값을 더해준다는 개념
+                    nums[b - 1] = c; // 원래 배열에도 바꿔줘야 다음 연산때 이용!
+                } else {
+                    sb.append(tree.sectionQuery(b, (int) c)).append('\n');
+                }
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
+
+/*
+[1]
+5 2 2
+1
+2
+3
+4
+5
+1 3 6
+2 2 5
+1 5 2
+2 3 3
+
+17
+6
+
+[2]
+4 0 1
+1
+2
+3
+4
+2 1 1
+
+1
+ */
