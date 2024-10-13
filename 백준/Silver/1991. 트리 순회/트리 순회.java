52c05ced940be9616f8886eeb1ac68be18050bee
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-10-13 13:16:02 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,9 +2,6 @@ import java.io.*;
 import java.util.*;
 
 /*
-백준 1991 트리 순회
-
-<풀이>
 
  */
 
@@ -21,55 +18,61 @@ class Node {
 }
 
 public class Main {
-    static void preTraverse(Node node) {
-        if (node != null) {
-            sb.append(node.cur);
-            preTraverse(node.left);
-            preTraverse(node.right);
-        }
-    }
-    static void midTraverse(Node node) {
-        if (node != null) {
-            midTraverse(node.left);
-            sb.append(node.cur);
-            midTraverse(node.right);
-        }
-    }
-    static void postTraverse(Node node) {
-        if (node != null) {
-            postTraverse(node.left);
-            postTraverse(node.right);
-            sb.append(node.cur);
+    static int N, M;
+    static Node[] tree;
+
+    static void preTraverse(Node node, StringBuilder sbInFunc) {
+        if (node != null)
+        {
+            sbInFunc.append(node.cur);
+            preTraverse(node.left, sbInFunc);
+            preTraverse(node.right, sbInFunc);
         }
     }
 
-    static StringBuilder sb = new StringBuilder();
+    static void midTraverse(Node node, StringBuilder sbInFunc) {
+        if (node != null)
+        {
+            midTraverse(node.left, sbInFunc);
+            sbInFunc.append(node.cur);
+            midTraverse(node.right, sbInFunc);
+        }
+    }
 
-    public static void main(String[]args) throws IOException {
+    static void postTraverse(Node node, StringBuilder sbInFunc) {
+        if (node != null)
+        {
+            postTraverse(node.left, sbInFunc);
+            postTraverse(node.right, sbInFunc);
+            sbInFunc.append(node.cur);
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        Node[] nodes = new Node[N];
-        for (int i = 0; i < N; i++) {
-            nodes[i] = new Node((char) ('A' + i), null, null);
-//            System.out.println("nodes[" + i + "] = " + (char) ('A' + 1 - i));
-        }
+        N = Integer.parseInt(br.readLine());
+        tree = new Node[N + 1];
+        for (int i = 0; i < N; i++) tree[i + 1] = new Node((char) ('A' + i), null, null);
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             char cur = st.nextToken().charAt(0);
             char left = st.nextToken().charAt(0);
             char right = st.nextToken().charAt(0);
-//            System.out.println(cur + " / " + left + " / " + right);
-            if (left != '.') nodes[cur - 'A'].left = nodes[left - 'A'];
-            if (right != '.') nodes[cur - 'A'].right = nodes[right - 'A'];
+            if (left != '.') tree[cur - 'A' + 1].left = tree[left - 'A' + 1];
+            if (right != '.') tree[cur - 'A' + 1].right = tree[right - 'A' + 1];
         }
-        preTraverse(nodes[0]);
+        preTraverse(tree[1], sb);
         sb.append('\n');
-        midTraverse(nodes[0]);
+        midTraverse(tree[1], sb);
         sb.append('\n');
-        postTraverse(nodes[0]);
+        postTraverse(tree[1], sb);
         System.out.println(sb.toString());
     }
 }
 
+/*
+[1]
+
+ */
