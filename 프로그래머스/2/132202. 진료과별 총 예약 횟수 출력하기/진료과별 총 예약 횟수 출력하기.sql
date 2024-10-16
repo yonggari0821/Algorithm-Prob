dce235c720b78ff5639e41b1925c4030b54edf7b
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2024-10-16 16:42:46 +0900

    [level 2] Title: 진료과별 총 예약 횟수 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -1,16 +1,16 @@
-# SELECT
-#     MCDP_CD AS "진료과코드",
-#     COUNT(*) AS "5월예약건수"
-# FROM
-#     APPOINTMENT
-# WHERE
-#     EXTRACT(YEAR FROM APNT_YMD) = 2022
-#     AND EXTRACT(MONTH FROM APNT_YMD) = 5
-# GROUP BY
-#     MCDP_CD
-# ORDER BY
-#     "5월예약건수" ASC,
-#     "진료과코드" ASC
+SELECT
+    MCDP_CD AS "진료과코드",
+    COUNT(*) AS "5월예약건수"
+FROM
+    APPOINTMENT
+WHERE
+    EXTRACT(YEAR FROM APNT_YMD) = 2022
+    AND EXTRACT(MONTH FROM APNT_YMD) = 5
+GROUP BY
+    MCDP_CD
+ORDER BY
+    5월예약건수 ASC,
+    진료과코드 ASC
 
 
 # SELECT
@@ -26,15 +26,15 @@
 #     "5월예약건수" ASC,
 #     "진료과코드" ASC
 
-SELECT
-    MCDP_CD AS '진료과코드',
-    COUNT(*) AS '5월예약건수'
-FROM
-    APPOINTMENT
-WHERE
-    DATE_FORMAT(APNT_YMD, '%Y-%m') = '2022-05'
-GROUP BY
-    MCDP_CD
-ORDER BY
-    COUNT(*) ASC,
-    MCDP_CD ASC
+# SELECT
+#     MCDP_CD AS '진료과코드',
+#     COUNT(*) AS '5월예약건수'
+# FROM
+#     APPOINTMENT
+# WHERE
+#     DATE_FORMAT(APNT_YMD, '%Y-%m') = '2022-05'
+# GROUP BY
+#     MCDP_CD
+# ORDER BY
+#     COUNT(*) ASC,
+#     MCDP_CD ASC
