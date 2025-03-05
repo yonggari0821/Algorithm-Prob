cced80cfd1e7707f43b3640eae049e09a494cd59
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-03-06 01:13:54 +0900

    [level 0] Title: 문자열 섞기, Time: 0.11 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> String {
-    var output : String = ""
-    for ( s1, s2 ) in zip ( str1, str2 ) {
-        output += String(s1) + String(s2)
-    }
-    return output
+    return zip(str1, str2).map{ (s1, s2) in String(s1) + String(s2) }.joined()
 }
