7159312922e9f280fa17b9e768d3362492cf56b2
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-03-07 16:51:17 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
@@ -1,15 +1,18 @@
 import Foundation
 
-func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
-    var diceSet : Set<Int> = [a, b, c]
-    switch (diceSet.count) {
-        case 3 :
-            return a + b + c
-        case 2 :
-            return (a*a + b*b + c*c) * (a + b + c)
-        case 1 :
-            return (a*a*a + b*b*b + c*c*c) * (a*a + b*b + c*c) * (a + b + c)
-        default :
-            return 0
+func solution(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    let count = Set([a, b, c]).count
+    var score = a + b + c
+    
+    func squareSum() -> Int { a * a + b * b + c * c }
+    func cubeSum() -> Int { a * a * a + b * b * b + c * c * c }
+    
+    if count <= 2 {
+        score *= squareSum()
     }
+    if count <= 1 {
+        score *= cubeSum()
+    }
+    
+    return score
 }
