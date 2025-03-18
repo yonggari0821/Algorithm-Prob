f7815960e729f27689de42a47f2879a52c54197a
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-03-18 23:55:32 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.00 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,27 @@
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Solution {
+    public static String solution(String s) {
+        String answer = "";
+
+        StringTokenizer numArr = new StringTokenizer(s," ", false);
+        StringBuilder answerstr = new StringBuilder();
+        TreeSet numTree = new TreeSet<>();
+        while(numArr.hasMoreTokens())
+            numTree.add(Integer.parseInt(numArr.nextToken().toString()));
+        System.out.println(numTree);
+        answerstr.append(numTree.first());
+        answerstr.append(" ");
+        answerstr.append(numTree.last());
+        System.out.println(answerstr);
+
+        answer = answerstr.toString();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "-1 -2 -3 -4";
+        System.out.println(solution(s));
+    }
+}
