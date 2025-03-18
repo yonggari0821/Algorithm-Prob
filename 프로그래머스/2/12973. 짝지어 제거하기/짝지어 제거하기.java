eb3c36786cec21cbc72f078a1c2054705cb96dc8
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-03-19 00:00:00 +0900

    [level 2] Title: 짝지어 제거하기, Time: 155.20 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {   
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty() || stack.peek() != c) {stack.push(c);}
+            else {stack.pop();}
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
