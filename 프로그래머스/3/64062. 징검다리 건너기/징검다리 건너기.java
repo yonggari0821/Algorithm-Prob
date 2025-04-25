e7dd1d61aa9b144d9f7f189aa50c0c135da9b13e
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-04-25 17:02:06 +0900

    [level 3] Title: 징검다리 건너기, Time: 33.24 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int left = 1;
+        int right = 200000000;
+        int answer = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (canCross(stones, k, mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    private boolean canCross(int[] stones, int k, int people) {
+        int skip = 0;
+        for (int stone : stones) {
+            if (stone < people) skip++;
+            else skip = 0;
+            if (skip >= k) return false;
+        }
+        return true;
+    }
+}
