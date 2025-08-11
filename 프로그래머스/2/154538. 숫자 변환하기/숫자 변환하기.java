44da9e5e21d578a32cce1bd575955d3100d44f2d
yonggari0821 <110471211+yonggari0821@users.noreply.github.com>
2025-08-11 21:25:00 +0900

    [level 2] Title: 숫자 변환하기, Time: 31.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,50 +1,17 @@
 import java.util.*;
 
 class Solution {
-    
-    private static int[][] dp;
-    private static final int MAX = 3000000;
-    
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        
-        dp = new int[y + 1][4];
-        
-        for (int r = 0; r <= y; r++) Arrays.fill(dp[r], MAX);
-        
-        dp[x][0] = 0;
-        dp[x][1] = 0;
-        dp[x][2] = 0;
-        dp[x][3] = 0;
-        
+        int[] dp = new int[y + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[x] = 0;
+
         for (int i = x; i <= y; i++) {
-            if (i >= x + n) {
-                dp[i][0] = Math.min(dp[i - n][3] + 1, dp[i][3]);
-            }
-            if (i >= 2 * x && i % 2 == 0) {
-                dp[i][1] = Math.min(dp[i/2][3] + 1, dp[i][3]);
-            }
-            if (i >= 3 * x && i % 3 == 0) {
-                dp[i][2] = Math.min(dp[i/3][3] + 1, dp[i][3]);
-            }
-            
-            dp[i][3] = Math.min(
-                Math.min(
-                    dp[i][0]
-                    , 
-                    Math.min(
-                        dp[i][1]
-                        , 
-                        dp[i][2]
-                    )
-                )
-                ,
-                dp[i][3]
-            );
+            if (dp[i] == Integer.MAX_VALUE) continue;
+            if (i + n <= y) dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+            if (i * 2 <= y) dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+            if (i * 3 <= y) dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
         }
-        
-        int min = MAX;
-        min = Math.min(dp[y][3], min);
-        return min == MAX ? -1 : min;
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
     }
 }
